61a2154f2be0029274148e76133ec66143b487dc
Juls <luigfmp@gmail.com>
2025-04-10 09:45:02 -0500

    feat: :sparkles: add FallingText and FadeContent components for enhanced text animations, update package.json with matter-js dependency

--- src/app/(home)/ui/Header/index.tsx
+++ src/app/(home)/ui/Header/index.tsx
@@ -1,5 +1,6 @@
 'use client'
 
+import FallingText from '@/shared/components/FallingText'
 import IconButton from '@/shared/ui/IconButton'
 import ShadowText from '@/shared/ui/ShadowText'
 import Social from '@/shared/ui/Social'
@@ -30,9 +31,20 @@ const Header: FC = () => {
 
         <section className='header-main'>
           <h1 className='header-title'>LUIS FERNANDO</h1>
-          <h5 className='header-description'>
+          <FallingText
+            text={`Desarrollador y diseñador de aplicaciones web Full Stack`}
+            highlightWords={['Full', 'Stack']}
+            highlightClass='highlighted'
+            trigger='hover'
+            backgroundColor='transparent'
+            wireframes={false}
+            gravity={0.56}
+            fontSize='2rem'
+            mouseConstraintStiffness={0.9}
+          />
+          {/* <h5 className='header-description'>
             Desarrollador y diseñador de aplicaciones web <u>Full Stack</u>
-          </h5>
+          </h5> */}
           <h2 className='header-nickname'>#MISHUM</h2>
           <p>Atento a tus menajes en:</p>
           <Social className='home-social' aria-label='Enlaces a redes sociales de Luis Fernando' />
