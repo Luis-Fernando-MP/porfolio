9b781bce7f09dbed4aa30502a09be515c92230fa
haui <luigfmp@gmail.com>
2025-08-03 09:54:11 -0500

    refactor: 🔧 Updated next.config.js to support unstable view transitions

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,10 @@ import { withContentlayer } from 'next-contentlayer2'
 
 const nextConfig: NextConfig = {
   allowedDevOrigins: ['127.0.0.1', '*.127.0.0.1', 'localhost'],
-  reactStrictMode: true
+  reactStrictMode: true,
+  experimental: {
+    viewTransition: true
+  }
 }
 
 export default withContentlayer(nextConfig)
